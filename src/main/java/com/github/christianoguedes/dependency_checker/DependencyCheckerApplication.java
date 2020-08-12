850fcba33d11114a771525ead6dfe90cd822c2d8
Guedes <christiao.guedes@zup.com.br>
2020-08-11 22:58:56 -0300

    dependency checker example project

---
+++ src/main/java/com/github/christianoguedes/dependency_checker/DependencyCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.github.christianoguedes.dependency_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyCheckerApplication.class, args);
+    }
+
+}
